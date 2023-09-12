fdef3c693729f7a5ddd48038667aeb4d0717d1f9
James-Mannie-Hammond <james.m.hammond-1@ou.edu>
2023-09-12 17:10:01 -0500

    Working user input and print to screen for computeSum and computeProduct fucntions

--- app.py
+++ app.py
@@ -16,3 +16,18 @@ def computeProduct(numbers):
 		total *= number
 	return total
 
+
+
+
+def getUserInput():
+	user_input = input("Enter the numbers")
+	numbers = [float(num) for num in user_input.split()]
+	return numbers
+
+
+numbers = getUserInput()
+sums = computeSum(numbers)
+product = computeProduct(numbers)
+
+print(f"The sum is: {sums}")
+print(f"The product is: {product}")
