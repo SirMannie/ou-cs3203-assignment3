ee87b3b35637da47324680233afbb684dad0bc4d
James-Mannie-Hammond <james.m.hammond-1@ou.edu>
2023-09-12 18:18:40 -0500

    Added reverse array order function

--- app.py
+++ app.py
@@ -16,18 +16,25 @@ def computeProduct(numbers):
 		total *= number
 	return total
 
+def rlist(numbers):
+	
+	#reverse = [float(num) for num in numbers.split()]
+	
+	return numbers[::-1]
 
 
 
 def getUserInput():
 	user_input = input("Enter the numbers")
-	numbers = [float(num) for num in user_input.split()]
+	numbers = [int(num) for num in user_input.split()]
 	return numbers
 
 
 numbers = getUserInput()
 sums = computeSum(numbers)
 product = computeProduct(numbers)
+reverse = rlist(numbers)
 
 print(f"The sum is: {sums}")
 print(f"The product is: {product}")
+print(f"The array in reverse is: {reverse}")
