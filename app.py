52883cf91d0fe761a5aa5c2812477fdd7bd53cbf
James-Mannie-Hammond <james.m.hammond-1@ou.edu>
2023-09-12 18:35:01 -0500

    Added main method for easy calling

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
 
 def computeSum(numbers):
-	
+
 	total = 0
 
 	for number in numbers:
 		total += number
+
 	return total
 
 
 def computeProduct(numbers):
 
 	total = 1
-	
+
 	for number in numbers:
 		total *= number
+
 	return total
 
 def rlist(numbers):
-	
-	#reverse = [float(num) for num in numbers.split()]
-	
+
+
+
 	return numbers[::-1]
 
 
@@ -29,12 +31,14 @@ def getUserInput():
 	numbers = [int(num) for num in user_input.split()]
 	return numbers
 
+def main():
+	numbers = getUserInput()
+	sums = computeSum(numbers)
+	product = computeProduct(numbers)
+	reverse = rlist(numbers)
 
-numbers = getUserInput()
-sums = computeSum(numbers)
-product = computeProduct(numbers)
-reverse = rlist(numbers)
+	print(f"The sum is: {sums}")
+	print(f"The product is: {product}")
+	print(f"The array in reverse is: {reverse}")
 
-print(f"The sum is: {sums}")
-print(f"The product is: {product}")
-print(f"The array in reverse is: {reverse}")
+main()
