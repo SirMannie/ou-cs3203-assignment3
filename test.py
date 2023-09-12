4a8c046f906151366ce445d0c39011523af93699
James-Mannie-Hammond <james.m.hammond-1@ou.edu>
2023-09-12 12:22:54 -0500

    Test cases added for compute product and adjusted previous test cases for simplicity

--- test.py
+++ test.py
@@ -1,15 +1,28 @@
 
 import app
 
-test_cases = [
+test_cases_sum = [
 	([1,2,3], 6),
 	([1,2,3,4,5], 15),
 	([4,5], 9)
 ]
 
-for test_value, expected_result in test_cases: 
-	actual_result = app.computeSum(test_value)
-	if app.computeSum(test_value) == expected_result:
-		print('Test passed')
+test_cases_product = [
+	([1,2,3], 6),
+	([1,2,3,4,5], 120),
+	([4,5], 20)
+]
+
+for test_value_sum, expected_result_sum in test_cases_sum: 
+	actual_result_sum = app.computeSum(test_value_sum)
+	if app.computeSum(test_value_sum) == expected_result_sum:
+		print('Test passed for sum')
 	else:
-		print(f'Test failed for input {test_value}: Expected {expected_result}, but got {actual_result}')
+		print(f'Test failed for sum input {test_value_sum}: Expected {expected_result_sum}, but got {actual_result_sum}')
+
+for test_value_product, expected_result_product in test_cases_product:
+	actual_result_product = app.computeProduct(test_value_product)
+	if app.computeProduct(test_value_product) == expected_result_product:
+		print('Test passed for product')
+	else:
+		print(f'Test failed for product input {test_value_product}: Expected {expected_result_product}, but got {actual_result_product}')
