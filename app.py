67e0b904e4ae329f52954747736cea482f9b08a8
James-Mannie-Hammond <james.m.hammond-1@ou.edu>
2023-09-13 17:37:36 -0500

    part 11

--- app.py
+++ app.py
@@ -1,47 +1,19 @@
 
-#computes the sum of an array of numbers
 def computeSum(numbers):
-
+	
 	total = 0
 
 	for number in numbers:
 		total += number
-
 	return total
 
+#adding comment for part 11
 
-#computes the product of an array of numbers
 def computeProduct(numbers):
 
 	total = 1
-
+	
 	for number in numbers:
 		total *= number
-
 	return total
 
-#reverses the given array
-def rlist(numbers):
-
-
-
-	return numbers[::-1]
-
-
-#takes user input
-def getUserInput():
-	user_input = input("Enter the numbers")
-	numbers = [int(num) for num in user_input.split()]
-	return numbers
-
-def main():
-	numbers = getUserInput()
-	sums = computeSum(numbers)
-	product = computeProduct(numbers)
-	reverse = rlist(numbers)
-
-	print(f"The sum is: {sums}")
-	print(f"The product is: {product}")
-	print(f"The array in reverse is: {reverse}")
-
-main()
